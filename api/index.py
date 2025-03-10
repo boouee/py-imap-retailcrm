c3444da7b691175e86901cc30d559363d025bba0
boouee <104202645+boouee@users.noreply.github.com>
2025-03-10 23:31:47 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -63,6 +63,14 @@ async def post_order(client, first_name, last_name, email, subject, text, html):
     
     #result = await client.post(url + 'files/upload', headers = headers)#, content = b'5567')# files = {'upload-file':attachments[0].payload}, headers = headers)
     #print(attachments[0].payload)
+    try:
+        print(customer["customers"])
+    except Exception as e:
+        print('exception: ', e)
+    try:
+        print(customer["customers"])
+    except Exception as e:
+        print('exception: ', e)
     print(customer)
     print('result: ', result)
     return customer 
