4f31e9a6b6f165fcae778eefd5becaef861a285c
boouee <104202645+boouee@users.noreply.github.com>
2025-03-11 20:51:39 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -50,7 +50,7 @@ async def main(client):
         for a in msg["attachments"]: 
             files = {'file': a.payload}
             try:
-                file = await client.post(url + '/api/v5/files/upload', files=files, headers=headers)
+                file = await client.post(url + '/api/v5/files/upload', payload=a.payload, headers=headers)
             except Exception as e:
                 print('exception: ', e)
             print(file.text)
